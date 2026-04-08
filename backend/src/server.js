16f41dee8dbd4919cfc7aaaf1b5e7c3bc2fc37fc
Ashu Singh <singhaashu271@gmail.com>
2026-04-08 11:12:53 +0530

    latest update

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const settingsRoutes = require('./routes/settingsRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const { startWorkers } = require('./workers/index');
 
